f07806bc2e357c1d84a96eaa8536f571855305a2
Jose Cabral <jose12marte@yahoo.com>
2022-01-17 22:24:45 -0500

    added new feature to have skills

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,28 +2,40 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Jose Cabral | Your DevOps Dev', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'A Latino man', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Jose Cabral',
+  subtitle: 'Your favorite DevOps Developer',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  img: 'myPhoto.jpg',
+  paragraphOne: `Born in the Dominican Republic, Jose Cabral always had a knack for computer and technology since a young age.
+  When he was 9 years old, he downloaded Adobe After Effects and experimented with video editing and effects. Although the videos
+  were terrible (this maybe subjective), the video sparked Jose's new found interest into computers and the internet. This new hobby
+  developed into interest in building computers, in which he built his first computer at 18 after saving up money from his part-time job.`,
+  paragraphTwo: `Eventually, Jose decided to go to school for something that is computer related after teaching himself basic Python starting 
+  at 19 years old. In May of 2021, he graduated from Georgia State University with a BBA in Computer Informaiton Systems (CIS) with a concentration
+  in Application Developement.`,
+  paragraphThree: `Today, Jose works at FIS as a DevOps Engineer learning, absorbing and implementing CI/CD concepts everyday. Some of his hobbies include
+  learning STEM subjects (currently learning Calculus and Physics), keeping his coding knowledge sharp, going to the gym and wathcing YouTube videos.`,
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
+// SKILLS DATA
+export const skillsData = [
+  //{skill1: "Python", image: "pathToImage.jpg", description: "Used Python in multiple jobs"}
+  //{skill1: "Python", image: "pathToImage.jpg", description: "Used Python in multiple jobs"}
+  //{skill1: "Python", image: "pathToImage.jpg", description: "Used Python in multiple jobs"}
+]
 // PROJECTS DATA
 export const projectsData = [
   {
